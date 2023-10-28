e35451e90b187f4519a7fa46df8bdac0957d853b
Sakshi <93873783+Sakshi0128@users.noreply.github.com>
2023-10-28 20:26:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// console.log("Running")
+
+
+
+document.querySelector('.wine').addEventListener("click", ()=> {
+    document.querySelector('.sidebar').classList.toggle ('Hide');
+
+    
+    });
+
+    
